46931439d2516a06b2499ef7858f21ade28ae79a
chen zhen <chzhen@localhost.localdomain>
2013-02-22 11:38:16 +0800

    change the time zone and database file directory

--- autofill/autofill/settings.py
+++ autofill/autofill/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'C:/Users/chzhen/Dropbox/openTaobao/db/autofill.db', # Or path to database file if using sqlite3.
+        'NAME': '/chzhen/openTaobao/db/autofill.db', # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -106,7 +106,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    'C:/Users/chzhen/Dropbox/openTaobao/autofill/template'
+    '/chzhen/openTaobao/autofill/template'
 )
 
 INSTALLED_APPS = (
